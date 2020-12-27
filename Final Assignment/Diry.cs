b7e2e9987713a6f3f86277b7d26783abbbbc4e04
Iamsharan001 <76622297+Iamsharan001@users.noreply.github.com>
2020-12-27 17:01:26 +0600

    DataAccess & Diry

---
+++ Final Assignment/Diry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Assignment
+{
+    class Diry
+    {
+        public int Number { set; get; }
+        public string Experience { set; get; }
+        public string Thought { set; get; }
+        public string Fellings { set; get; }
+        public string Importance { set; get; }
+        public string EventDate { set; get; }
+
+        public string Picture { set; get; }
+
+    }
+}
